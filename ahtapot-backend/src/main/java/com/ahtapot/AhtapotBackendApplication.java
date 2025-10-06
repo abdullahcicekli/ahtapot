abab3693344a1f0c36d11ed07cd5f92b8c386a7d
Abdullah Çiçekli <abdulahcicekli@gmail.com>
2025-10-07 00:32:28 +0300

    main | initial commit for ahtapot

---
+++ ahtapot-backend/src/main/java/com/ahtapot/AhtapotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ahtapot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhtapotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhtapotBackendApplication.class, args);
+	}
+
+}
---
+++ ahtapot-backend/src/test/java/com/ahtapot/AhtapotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahtapot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhtapotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
